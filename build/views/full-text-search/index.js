0ecc0fec733a2a592471a721b8771f211f72f181
Gijsjan <agijsbro@gmail.com>
2019-11-01 11:26:07 +0100

    Add loader to full text input

--- build/views/full-text-search/index.js
+++ build/views/full-text-search/index.js
@@ -13,10 +13,17 @@ exports.Wrapper = styled_1.default.div `
 	grid-template-columns: auto 40px;
 	position: relative;
 
-	div {
+	& > .search-icon {
 		align-self: center;
 		fill: #BBB;
 		padding-right: 16px;
+	}
+
+	#loader {
+		background: linear-gradient(to right, white 0%, #AAA 80%, white 100%);
+		grid-column: 1 / span 2;
+		height: 3px;
+		width: 0;
 	}
 `;
 exports.Input = styled_1.default.input `
@@ -29,6 +36,8 @@ exports.Input = styled_1.default.input `
 class FullTextSearch extends React.PureComponent {
     constructor() {
         super(...arguments);
+        this.loaderRef = React.createRef();
+        this.loaderIntervalProgress = 0;
         this.state = {
             suggest: false,
             value: ''
@@ -38,9 +47,10 @@ class FullTextSearch extends React.PureComponent {
             this._addQuery(query);
         };
         this._addQuery = (query) => {
+            this.hideLoader();
             this.props.state.facetsManager.addQuery(query);
         };
-        this.requestAddQuery = lodash_debounce_1.default(this._addQuery, 300);
+        this.requestAddQuery = lodash_debounce_1.default(this._addQuery, 1000);
     }
     render() {
         return (React.createElement(exports.Wrapper, null,
@@ -49,14 +59,30 @@ class FullTextSearch extends React.PureComponent {
                         suggest: true,
                         value: ev.target.value
                     });
+                    this.showLoader();
                     this.requestAddQuery(ev.target.value);
                 }, onClick: () => this.setState({ suggest: false }), placeholder: "Search", value: this.state.value }),
-            React.createElement("div", null,
+            React.createElement("div", { className: "search-icon" },
                 React.createElement("svg", { viewBox: "0 0 250.313 250.313" },
                     React.createElement("path", { d: "M244.186,214.604l-54.379-54.378c-0.289-0.289-0.628-0.491-0.93-0.76 c10.7-16.231,16.945-35.66,16.945-56.554C205.822,46.075,159.747,0,102.911,0S0,46.075,0,102.911 c0,56.835,46.074,102.911,102.91,102.911c20.895,0,40.323-6.245,56.554-16.945c0.269,0.301,0.47,0.64,0.759,0.929l54.38,54.38 c8.169,8.168,21.413,8.168,29.583,0C252.354,236.017,252.354,222.773,244.186,214.604z M102.911,170.146 c-37.134,0-67.236-30.102-67.236-67.235c0-37.134,30.103-67.236,67.236-67.236c37.132,0,67.235,30.103,67.235,67.236 C170.146,140.044,140.043,170.146,102.911,170.146z" }))),
             this.props.autoSuggest != null &&
                 this.state.suggest &&
-                React.createElement(auto_suggest_1.default, { autoSuggest: this.props.autoSuggest, onClick: this.addQuery, value: this.state.value })));
+                React.createElement(auto_suggest_1.default, { autoSuggest: this.props.autoSuggest, onClick: this.addQuery, value: this.state.value }),
+            React.createElement("div", { id: "loader", ref: this.loaderRef })));
+    }
+    showLoader() {
+        clearInterval(this.loaderIntervalID);
+        this.loaderIntervalProgress = 0;
+        this.loaderIntervalID = setInterval(() => {
+            this.loaderIntervalProgress += 25;
+            const perc = this.loaderIntervalProgress / 1000;
+            this.loaderRef.current.style.width = `${perc * 100}%`;
+        }, 25);
+    }
+    hideLoader() {
+        clearInterval(this.loaderIntervalID);
+        this.loaderIntervalProgress = 0;
+        this.loaderRef.current.style.width = '0';
     }
 }
 exports.default = React.forwardRef((props, ref) => (React.createElement(context_1.default.Consumer, null, state => React.createElement(FullTextSearch, Object.assign({}, props, { ref: ref, state: state })))));
--- src/views/full-text-search/index.tsx
+++ src/views/full-text-search/index.tsx
@@ -12,10 +12,17 @@ export const Wrapper = styled.div`
 	grid-template-columns: auto 40px;
 	position: relative;
 
-	div {
+	& > .search-icon {
 		align-self: center;
 		fill: #BBB;
 		padding-right: 16px;
+	}
+
+	#loader {
+		background: linear-gradient(to right, white 0%, #AAA 80%, white 100%);
+		grid-column: 1 / span 2;
+		height: 3px;
+		width: 0;
 	}
 `
 export const Input = styled.input`
@@ -37,6 +44,10 @@ interface State {
 	value: string
 }
 class FullTextSearch extends React.PureComponent<Props & ConsumerProps, State> {
+	loaderRef: React.RefObject<HTMLDivElement> = React.createRef()
+	loaderIntervalID: number
+	loaderIntervalProgress = 0
+
 	state = {
 		suggest: false,
 		value: ''
@@ -52,13 +63,14 @@ class FullTextSearch extends React.PureComponent<Props & ConsumerProps, State> {
 							suggest: true,
 							value: ev.target.value
 						})
+						this.showLoader()
 						this.requestAddQuery(ev.target.value)
 					}}
 					onClick={() => this.setState({ suggest: false })}
 					placeholder="Search"
 					value={this.state.value}
 				/>
-				<div>
+				<div className="search-icon">
 					<svg viewBox="0 0 250.313 250.313">
 						<path d="M244.186,214.604l-54.379-54.378c-0.289-0.289-0.628-0.491-0.93-0.76 c10.7-16.231,16.945-35.66,16.945-56.554C205.822,46.075,159.747,0,102.911,0S0,46.075,0,102.911 c0,56.835,46.074,102.911,102.91,102.911c20.895,0,40.323-6.245,56.554-16.945c0.269,0.301,0.47,0.64,0.759,0.929l54.38,54.38 c8.169,8.168,21.413,8.168,29.583,0C252.354,236.017,252.354,222.773,244.186,214.604z M102.911,170.146 c-37.134,0-67.236-30.102-67.236-67.235c0-37.134,30.103-67.236,67.236-67.236c37.132,0,67.235,30.103,67.235,67.236 C170.146,140.044,140.043,170.146,102.911,170.146z" />
 					</svg>
@@ -72,6 +84,7 @@ class FullTextSearch extends React.PureComponent<Props & ConsumerProps, State> {
 						value={this.state.value}
 					/>	
 				}
+				<div id="loader" ref={this.loaderRef} />
 			</Wrapper>
 		)
 	}
@@ -84,9 +97,27 @@ class FullTextSearch extends React.PureComponent<Props & ConsumerProps, State> {
 
 
 	private _addQuery = (query: string) => {
+		this.hideLoader()
 		this.props.state.facetsManager.addQuery(query)
 	}
-	private requestAddQuery = debounce(this._addQuery, 300)
+
+	private showLoader() {
+		clearInterval(this.loaderIntervalID)
+		this.loaderIntervalProgress = 0
+		this.loaderIntervalID = setInterval(() => {
+			this.loaderIntervalProgress += 25
+			const perc = this.loaderIntervalProgress / 1000
+			this.loaderRef.current.style.width = `${perc * 100}%`
+		}, 25)
+	}
+
+	private hideLoader() {
+		clearInterval(this.loaderIntervalID)
+		this.loaderIntervalProgress = 0
+		this.loaderRef.current.style.width = '0'
+	}
+
+	private requestAddQuery = debounce(this._addQuery, 1000)
 }
 
 
